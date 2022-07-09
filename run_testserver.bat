46cde018f68953ee14f7d475b3c56b40ab0df024
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2022-07-10 01:38:37 +0200

    Create run_testserver.bat

---
+++ run_testserver.bat
@@ -0,0 +1,8 @@
+
+copy /b/v/y target\Minecraft-Konziiis-Base-Defence-1.0-SNAPSHOT.jar testserver\plugins\Minecraft-Konziiis-Base-Defence-1.0-SNAPSHOT.jar
+
+cd testserver
+
+C:\Users\Marco\.jdks\corretto-17.0.3\bin\java -Xmx1G -jar paper-1.19-43.jar
+
+PAUSE
